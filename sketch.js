33619f49ac03136aa6562441c2464d469da03854
archana-1 <37152462+archana-1@users.noreply.github.com>
2022-10-26 18:48:02 +0530

    -

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ var allPlayers, car1, car2, fuels, powerCoins, obstacles;
 var cars = [];
 
 function preload() {
-  backgroundImage = loadImage("assets/background.png");
+  backgroundImage = loadImage("../assets/background.png");
   car1_img = loadImage("assets/car1.png");
   car2_img = loadImage("assets/car2.png");
   track = loadImage("assets/track.jpg");
